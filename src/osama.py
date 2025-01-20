62e06692285b0514640f5b1748c4ede62e264c81
dimi <120773637+dalpaka@users.noreply.github.com>
2025-01-20 09:29:01 +0100

    Create osama.py

---
+++ src/osama.py
@@ -0,0 +1,54 @@
+"""
+zlib License
+
+(C) 2024 Dimitri Jordan, Samy Gillman
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including non-commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+4. You may not use this Software for commercial use including redistribution
+   or any fork of such. This priduct is purely open source and non-sellable.
+"""
+bombs = 21
+isBombed = False
+isPakistan = True
+isOsama = True
+pakistan = "ð½"
+nigger = "what victim will be raped? :"
+faggot = False
+terrorist = {bombs,isBombed,isPakistan,isOsama}
+
+def rape(victim):
+    print(victim + "wasraped")
+
+def kill(victim):
+    print(victim + "waskilled")
+
+if nigger == "black":
+    newVictim = input(nigger)
+    rape(newVictim)
+
+for i in range(100):
+    if i / 4:
+        faggot=True
+    if faggot:
+        kill("faggot")
+
+def bomb (victim):
+    print (victim + "was bombed")
+
+if not isOsama:
+    isPakistan = False
+  
